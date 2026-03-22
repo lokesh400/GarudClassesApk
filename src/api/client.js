82b0b386606a106294822459c4ecfbed9dd95d43
Lokesh <lokeshbadgujjar400@gmail.com>
2026-03-22 19:01:04 +0530

    asd

--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const COOKIE_KEY = 'session_cookie';
 
-const API_BASE_URL = 'http://10.191.44.198:5000/api';
+// const API_BASE_URL = 'http://10.191.44.198:5000/api';
+
+const API_BASE_URL = 'https://testportal.garudclasses.com/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
